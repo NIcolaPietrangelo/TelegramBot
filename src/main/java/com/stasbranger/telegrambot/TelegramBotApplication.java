c45ee782f914130de991981603d91a87974994cf
nicola <nicola@nicola-OMEN>
2019-02-18 18:11:01 +0100

    now goes online

--- src/main/java/com/stasbranger/telegrambot/TelegramBotApplication.java
+++ src/main/java/com/stasbranger/telegrambot/TelegramBotApplication.java
@@ -2,12 +2,30 @@ package com.stasbranger.telegrambot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
+
+import com.stasbranger.telegrambot.service.TelegramSeviceImpl;
 
 @SpringBootApplication
-public class TelegramBotApplication {
+public class TelegramBotApplication 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		SpringApplication.run(TelegramBotApplication.class, args);
+		
+		ApiContextInitializer.init();
+		TelegramBotsApi botApi = new TelegramBotsApi();
+		
+		try {
+			botApi.registerBot(new TelegramSeviceImpl());
+		} catch (TelegramApiRequestException e) {
+			e.printStackTrace();
+		}
 	}
+	
 
 }
